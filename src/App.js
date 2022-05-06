8abe0ceef4f84c599802721246ecc172d3d9c9a6
lee jin gyeong <jenjennie707@gmail.com>
2022-05-07 02:43:09 +0900

    made 1st main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div>굥이 리액트 시작</div>
+      </div>
+      <div className= "banner"> 
+          <div className="banner-txt">
+          <h1> 냐하항 내일 컴포넌트 하구 불투명 박스 만들어야쥠</h1>
+          </div>
+        </div>
+
+        <div className="line">
+          <img></img>
+        </div>
     </div>
+    
   );
 }
 
 export default App;
+
+
