86c0309991336d702c010d59dd76112ab0485160
lee jin gyeong <jenjennie707@gmail.com>
2022-05-07 19:35:55 +0900

    Modifying_mainpage

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" meta name="viewport" content="width=device-width, initial-scale=1">
       <div className='black-nav'>
         <div>굥이 리액트 시작</div>
       </div>
       <div className= "banner"> 
           <div className="banner-txt">
-          <h1> 냐하항 내일 컴포넌트 하구 불투명 박스 만들어야쥠</h1>
+          <h4> #바다 #일출 #떠나자 #가볼만한곳</h4>
+          <h1>나만 보기 아까운 PLACE!</h1>
+          <h1>함께 공유하세요.</h1>
           </div>
-        </div>
+          </div>
+      <div className= "banner2"> 
+      <div className="banner-txt2">
+      <h1>당신의 장소에 사람들을 초대해보세요.</h1>
+      <h1>OMC가 도와드리겠습니다.</h1>
+      </div>
+      </div>
 
-        <div className="line">
-          <img></img>
-        </div>
     </div>
+    
     
   );
 }
