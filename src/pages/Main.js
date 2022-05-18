5412d5b08d780624c432c1f75d28c045d4b341f7
lee jin gyeong <jenjennie707@gmail.com>
2022-05-18 21:14:37 +0900

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,93 @@
 import React,{ useEffect, useRef, useState } from 'react';
 import './Main.scss';
 import Dots from "./Dots";
+import { BsSearch } from "react-icons/bs"
+import { Autoplay,Navigation, Pagination, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import image1 from '../img/travel-g61fc92606_1920.jpg'
+import image2 from '../img/barn-gc2cd1319e_1920.jpg'
+import image3 from '../img/camp-gd607affa9_1920.jpg'
+import image4 from '../img/landscape-g467b0ad9b_1920.jpg'
+import image5 from '../img/trees-gc20135b1a_1920.jpg'
+import image6 from '../img/utah-g543009de6_1920.jpg'
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const SwiperComponent =  () => {
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Autoplay, Navigation, Pagination,  A11y]}
+      spaceBetween={45}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      loop
+      // autoplay={{
+      //   delay: 2000,
+      //   disableOnInteraction: false
+      // }}
+      centeredSlides={true}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      {/* 나중에 map 처리 */}
+      <SwiperSlide>
+        <div className='swiper_div'>
+          <img className='image' src={image1} alt="image1"></img>
+          <div className="cardTitle">산과 바다와 연인과 함께</div>
+          <div className="tag"></div>
+          <div className="place"></div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className='swiper_div'>
+          <img className='image' src={image2} alt="image2"></img>
+          <div className="cardTitle">산과 바다와 연인과 함께</div>
+          <div className="tag"></div>
+          <div className="place"></div>
+        </div>
+      </SwiperSlide>  
+      <SwiperSlide>
+        <div className='swiper_div'>
+          <img className='image' src={image3} alt="image3"></img>
+          <div className="cardTitle">산과 바다와 연인과 함께</div>
+          <div className="tag"></div>
+          <div className="place"></div>
+        </div>      
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className='swiper_div'>
+          <img className='image' src={image4} alt="image4"></img>
+          <div className="cardTitle">산과 바다와 연인과 함께</div>
+          <div className="tag"></div>
+          <div className="place"></div>
+        </div>      
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className='swiper_div'>
+          <img className='image' src={image5} alt="image5"></img>
+          <div className="cardTitle">산과 바다와 연인과 함께</div>
+          <div className="tag"></div>
+          <div className="place"></div>
+        </div>      
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className='swiper_div'>
+          <img className='image' src={image6} alt="image6"></img>
+          <div className="cardTitle">산과 바다와 연인과 함께</div>
+          <div className="tag"></div>
+          <div className="place"></div>
+        </div>      
+      </SwiperSlide>    
+      
+
+
+    </Swiper>
+  );
+};
 
 const Main = () => {
   const outerDivRef = useRef();
@@ -86,7 +173,7 @@ const Main = () => {
           // 현재 3페이지
           console.log("현재 3페이지, up");
           outerDivRef.current.scrollTo({
-            top: pageHeight*2 + DIVIDER_HEIGHT*2,
+            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
             left: 0,
             behavior: "smooth"
           });
@@ -106,12 +193,35 @@ const Main = () => {
       <div ref={outerDivRef} className="outer">
         <Dots scrollIndex={scrollIndex} />
         <div className="inner bg_1">
-          <div className="search_div">
+          <div className="content">
+            <div className="search">
+              <input placeholder='원하시는 차박지를 검색하세요.'></input>
+              <div className="icon"><BsSearch /></div>
+            </div>
+            <div className="title"><p>Oh! My Car</p></div>
+            <div className="sub_title"><p>Let`s travel with my Car</p></div>
           </div>
-          <div className="title">Oh! My Car</div>
         </div>
         <div className="divider"></div>
-        <div className="inner bg_2">2</div>
+
+        <div className="inner bg_2">
+
+          <div className="content">
+            <div className="title">
+              <p>이런 여행지는 어떠세요?</p>
+              <div className="button">
+                <p>더 알아보기</p>
+                <div className="icon"><BsSearch /></div>
+              </div>
+            </div>
+            <div className="swiper">
+              <SwiperComponent />
+            </div>
+            
+            
+          </div>
+
+        </div>
         <div className="divider"></div>
         <div className="inner bg_3">3</div>
         <div className="divider"></div>
